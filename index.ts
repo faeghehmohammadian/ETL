c3cde87f289c5e390c4cac2952762ffc4eb38373
Faegheh <59620602+faeghehmohammadian@users.noreply.github.com>
2024-01-10 00:24:53 +0400

    add typescript

---
+++ index.ts
@@ -0,0 +1,12 @@
+export function transform(input: {
+    [key: string]: string[]
+  }): { [key: string]: number } {
+    const result: { [key: string]: number } = {}
+    for (const key in input) {
+      for (const char of input[key]) {
+        result[char.toLowerCase()] = parseInt(key, 10)
+      }
+    }
+    return result
+  }
+  export default transform
